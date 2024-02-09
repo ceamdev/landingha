c394eca7ff92fcb64b2d1829d602d3de6657a1cc
Carlos Astudillo <ceamdeveloper@gmail.com>
2024-02-09 14:56:22 -0400

    Trabajando en tema de Landing app.css y app.js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,3 +3,12 @@
 @section('content')
     <h1>Vista Home desde View, y enviada por el controlador HomeController y Plantilla.</h1>
 @endsection
+@section('testimoniostexto')
+    <section class="row">
+        <cite> 
+        <h2 class="col-12">Carlos Astudillo - <small>Diseño Gráfico</small></h2>
+            <p class="col-6 row"><img src="../resources/imgs/carlos.png" alt="Carlos Astudillo" class="img-fluid col-3" width="100px" height="auto" border="0">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam repellendus eius itaque, debitis maxime rerum blanditiis, distinctio molestiae esse et, odit facere minima inventore? Expedita maxime numquam suscipit voluptas maiores.</p>
+        </cite>
+    </section>
+@endsection
--- resources/views/layaouts/plantilla.blade.php
+++ resources/views/layaouts/plantilla.blade.php
@@ -6,13 +6,34 @@
     <title>@yield('title') - Harmony Academy - ErickMaldonado.us</title>
     <!-- favicon -->
     <!-- style css -->
+    <link rel="stylesheet" href="../resources/css/app.css">
 </head>
 <body>
     <!-- header -->
+    <header class="container">
+        <h1>Harmony Academy</h1>
+    </header>
     <!-- nav -->
+    <nav class="navbar">
+        <ul>
+            <li><a href="./">Inicio</a></li>
+            <li><a href="#testimonios">Testimonios</a></li>
+            <li><a href="cursos">Cursos</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+        </ul>
+    </nav>
+    <main class="container">
     <!-- <h1>Vista Home desde View, y enviada por el controlador HomeController.</h1> -->
     @yield('content')
+    @yield('testimoniostexto')
+    @yield('testimoniosvideo')
+    </main>
     <!-- footer -->
+    <footer class="container">
+        &copy; 2023 - <?php echo date('Y'); ?> - Derechos Reservados - Harmony Academy - Erick Maldonado 
+    </footer>
     <!-- script -->
+    <script src="../resources/js/app.js"></script>
+    <script src="../resources/js/bootstrap.js"></script>
 </body>
 </html>
